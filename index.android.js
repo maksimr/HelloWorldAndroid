ab80bb0c61bc1a9a32569133d5c6375c8639a54b
Maksim Ryzhikov <rv.maksim@gmail.com>
2015-12-31 12:43:04 +0300

    chore: add eslint

--- index.android.js
+++ index.android.js
@@ -18,44 +18,45 @@ var {
   StyleSheet,
   Text,
   View,
-  Image,
-} = React;
+  Image
+  } = React;
 
 var HelloWorldAndroid = React.createClass({
   render: function() {
+    var styles = HelloWorldAndroid.styles;
     var movie = MOCKED_MOVIES_DATA[0];
 
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
         <Text style={styles.welcome}>{movie.title}</Text>
         <Text style={styles.instructions}>{movie.year}</Text>
-        <Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}} />
-        </View>
-        );
+        <Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}}/>
+      </View>
+    );
   }
 });
 
-var styles = StyleSheet.create({
+HelloWorldAndroid.styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
   thumbnail: {
     width: 53,
-    height: 81,
-  },
+    height: 81
+  }
 });
 
 AppRegistry.registerComponent('HelloWorldAndroid', () => HelloWorldAndroid);
