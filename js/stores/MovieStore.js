4b1d23849adb01d5776b758292185f6bb34291b2
Maksim Ryzhikov <rv.maksim@gmail.com>
2016-01-09 16:47:40 +0300

    refactor: load real data

--- js/stores/MovieStore.js
+++ js/stores/MovieStore.js
@@ -1,6 +1,11 @@
 class MovieStore {
   static getAll() {
-    var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+    var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+
+    var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+    var PAGE_SIZE = 15;
+    var QUERY_PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+    var REQUEST_URL = API_URL + QUERY_PARAMS;
 
     return fetch(REQUEST_URL)
       .then((response) => response.json());
